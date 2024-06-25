8d5e0c2f4f00e30990d315e34dd1cabe9fc61416
J.T. Kalkman <j.t.kalkman@gmail.com>
2024-06-25 14:47:33 +0100

    Updates DiskSpaceInode.

--- SemontoFramework/src/ServerHealth/tests/DiskSpaceInode.php
+++ SemontoFramework/src/ServerHealth/tests/DiskSpaceInode.php
@@ -22,9 +22,41 @@ class DiskSpaceInode extends ServerHealthTest
 
         $error = false;
         $warning = false;
-
         $descriptions = [];
         $values = [];
+        $status = false;
+
+        $disk_usage = [];
+        try {
+            exec("df -i 2>&1", $disk_usage);
+        } catch (\Throwable $th) {
+            return new ServerHealthResult(
+                $this->name,
+                ServerStates::error,
+                'Failed to get the inode disk usage. (' . $th->getMessage() . ')', 
+                null
+            );
+        }
+
+        if (count($disk_usage) <= 1) {
+            return new ServerHealthResult($this->name, ServerStates::error, 'Failed to get the inode disk usage.', null);
+        }
+
+        $available_disks = [];
+        for ($i = 1; $i < count($disk_usage); $i++) {
+            $data = explode(" ", $disk_usage[$i]);
+
+            $proc = explode("%", $disk_usage[$i]);
+            $proc = $proc[0];
+            $proc = explode(" ", $proc);
+            $proc = (float) $proc[count($proc) - 1];
+
+            $available_disks[] = [
+                'name' => $data[0],
+                'proc' => $proc,
+                'description' => $data[0] . " (" . $proc . "%)"
+            ];
+        }
 
         foreach ($this->config['disks'] as $disk) {
             if (!isset($disk['name']) || !$disk['name']) {
@@ -33,32 +65,22 @@ class DiskSpaceInode extends ServerHealthTest
                 continue;
             }
 
-            $disk_space_inode = [];
-            try {
-                exec("df -i " . $disk['name'] . " 2>&1", $disk_space_inode);
-            } catch (\Throwable $th) {
+            $disk_usage = array_values(array_filter($available_disks, function ($available_disk) use ($disk) {
+                return $available_disk['name'] == $disk['name'];
+            }));
+
+            if (count($disk_usage) !== 1) {
                 $warning = true;
-                $descriptions[] = 'Failed to get the inode disk usage for ' . $disk['name'] . ' (' . $th->getMessage() . ')';
-                continue;
-            }
-
-            if (count($disk_space_inode) > 1) {
-                $percentage = explode("%", $disk_space_inode[1]);
-                $percentage = $percentage[0];
-                $percentage = explode(" ", $percentage);
-                $percentage = (float) $percentage[count($percentage) - 1];
-
-                $values[] = $percentage;
-                $descriptions[] = $disk['name'] . " (" . $percentage . "%)";
-
+                $descriptions[] = 'Failed to get the inode disk usage for ' . $disk['name'];
+            } else {
+                $disk_usage = $disk_usage[0];
                 $warning_percentage_threshold = isset($disk['warning_percentage_threshold']) ? $disk['warning_percentage_threshold'] : 75;
                 $error_percentage_threshold = isset($disk['error_percentage_threshold']) ? $disk['error_percentage_threshold'] : 90;
 
-                if ($percentage > $warning_percentage_threshold) $warning = true;
-                if ($percentage > $error_percentage_threshold) $error = true;
-            } else {
-                $warning = true;
-                $descriptions[] = 'Failed to get the inode disk usage for ' . $disk['name'] . ' (' . $disk_space_inode[0] . ')';
+                $values[] = $disk_usage['proc'];
+                $descriptions[] = $disk_usage['description'];
+                if ($disk_usage['proc'] > $warning_percentage_threshold) $warning = true;
+                if ($disk_usage['proc'] > $error_percentage_threshold) $error = true;
             }
         }
 
