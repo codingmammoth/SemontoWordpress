929c2377c64fd84d015167163da9210acf12c347
J.T. Kalkman <j.t.kalkman@gmail.com>
2024-06-26 10:05:53 +0100

    Makes DiskSpace test available only when exec is allowed. Improves input min values.

--- semonto-health-monitor__disk-space.php
+++ semonto-health-monitor__disk-space.php
@@ -6,57 +6,64 @@
         Ut officiis amet est labore et et itaque suscipit. Quas dicta pariatur doloremque voluptas accusamus.
     </p>
 
-    <div class="input-fields-text">
-        <p class="title-test">Disk space</p>
-        <div class="switch-option">
-            <label class="switch">
-                <input
-                type="checkbox"
-                name="semonto_disk_space_enable"
-                value="1" <?php checked(1, get_option('semonto_disk_space_enable')); ?>
-                />
-                <span class="slider round"></span>
-            </label>
-        </div>
-    </div>
-
-    <div class="warning-error">
-        <div class="warning one">Warning percentage</div>
-        <div class="warning">Error percentage</div>
-    </div>
-
-    <?php foreach ($disks as $disk_name => $disk_config ): ?>
-        <div class="input-fields">
-            <div class="input-fields-text">
-                <p class="title-test"><?php echo $disk_name; ?>:</p>
-                    <div class="switch-option">
-                        <label class="switch">
-                            <input
-                                type="checkbox"
-                                name="semonto_disk_space_config[<?php echo $disk_name; ?>][enabled]"
-                                value="1" <?php checked(1, $disk_config['enabled']); ?>
-                            />
-                            <span class="slider round"></span>
-                        </label>
-                    </div>
+    <?php if($shell_exec_available) : ?>
+        <div class="input-fields-text">
+            <p class="title-test">Disk space</p>
+            <div class="switch-option">
+                <label class="switch">
                     <input
-                        type="number"
-                        min="1"
-                        max="100"
-                        name="semonto_disk_space_config[<?php echo $disk_name; ?>][warning_percentage_threshold]"
-                        value="<?php echo esc_attr(intval($disk_config['warning_percentage_threshold'])); ?>"
-                        class="semonto_serverload"
+                    type="checkbox"
+                    name="semonto_disk_space_enable"
+                    value="1" <?php checked(1, get_option('semonto_disk_space_enable')); ?>
                     />
-                    <input
-                        type="number"
-                        min="1"
-                        max="100"
-                        name="semonto_disk_space_config[<?php echo $disk_name; ?>][error_percentage_threshold]"
-                        value="<?php echo esc_attr(intval($disk_config['error_percentage_threshold'])); ?>"
-                        class="semonto_serverload"
-                    />
+                    <span class="slider round"></span>
+                </label>
             </div>
         </div>
-    <?php endforeach; ?>
+
+        <div class="warning-error">
+            <div class="warning one">Warning percentage</div>
+            <div class="warning">Error percentage</div>
+        </div>
+
+        <?php foreach ($disks as $disk_name => $disk_config ): ?>
+            <div class="input-fields">
+                <div class="input-fields-text">
+                    <p class="title-test"><?php echo $disk_name; ?>:</p>
+                        <div class="switch-option">
+                            <label class="switch">
+                                <input
+                                    type="checkbox"
+                                    name="semonto_disk_space_config[<?php echo $disk_name; ?>][enabled]"
+                                    value="1" <?php checked(1, $disk_config['enabled']); ?>
+                                />
+                                <span class="slider round"></span>
+                            </label>
+                        </div>
+                        <input
+                            type="number"
+                            min="0"
+                            max="100"
+                            name="semonto_disk_space_config[<?php echo $disk_name; ?>][warning_percentage_threshold]"
+                            value="<?php echo esc_attr(intval($disk_config['warning_percentage_threshold'])); ?>"
+                            class="semonto_serverload"
+                        />
+                        <input
+                            type="number"
+                            min="0"
+                            max="100"
+                            name="semonto_disk_space_config[<?php echo $disk_name; ?>][error_percentage_threshold]"
+                            value="<?php echo esc_attr(intval($disk_config['error_percentage_threshold'])); ?>"
+                            class="semonto_serverload"
+                        />
+                </div>
+            </div>
+        <?php endforeach; ?>
+    <?php else : ?>
+        <p class="semonto-health-monitor__not-available">
+            <span>ℹ️</span>
+            This test is not available due to your server setup.
+        </p>
+    <?php endif; ?>
 
 </div>
