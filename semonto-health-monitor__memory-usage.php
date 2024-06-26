8a7f59cc1de404694cd9107b20f1e62ce242b2b3
J.T. Kalkman <j.t.kalkman@gmail.com>
2024-06-26 10:08:23 +0100

    Makes memory usage test only available when exec is allowed. Corrects input min values.

--- semonto-health-monitor__memory-usage.php
+++ semonto-health-monitor__memory-usage.php
@@ -4,40 +4,48 @@
         Get notified when the memory usage on your server is too high
     </p>
 
-    <div class="warning-error">
-        <div class="warning one">Warning threshold</div>
-        <div class="warning">Error threshold</div>
-    </div>
-
-    <div class="input-fields">
-        <div class="input-fields-text">
-            <p class="title-test">Memory usage:</p>
-            <div class="switch-option">
-                <label class="switch">
-                    <input
-                        type="checkbox"
-                        name="semonto_memory_usage_enable"
-                        value="1" <?php checked(1, get_option('semonto_memory_usage_enable')); ?>
-                    />
-                    <span class="slider round"></span>
-                </label>
-            </div>
-            <input
-                type="number"
-                min="1"
-                max="100"
-                name="semonto_memory_usage_enable_warning_threshold"
-                value="<?php echo esc_attr(intval(get_option('semonto_memory_usage_enable_warning_threshold', 90))); ?>"
-                class="semonto_serverload"
-            />
-            <input
-                type="number"
-                min="1"
-                max="100"
-                name="semonto_memory_usage_enable_error_threshold"
-                value="<?php echo esc_attr(intval(get_option('semonto_memory_usage_enable_error_threshold', 95))); ?>"
-                class="semonto_serverload"
-            />
+    <?php if($exec_available) : ?>
+        <div class="warning-error">
+            <div class="warning one">Warning threshold</div>
+            <div class="warning">Error threshold</div>
         </div>
-    </div>
+
+        <div class="input-fields">
+            <div class="input-fields-text">
+                <p class="title-test">Memory usage:</p>
+                <div class="switch-option">
+                    <label class="switch">
+                        <input
+                            type="checkbox"
+                            name="semonto_memory_usage_enable"
+                            value="1" <?php checked(1, get_option('semonto_memory_usage_enable')); ?>
+                        />
+                        <span class="slider round"></span>
+                    </label>
+                </div>
+                <input
+                    type="number"
+                    min="0"
+                    max="100"
+                    name="semonto_memory_usage_enable_warning_threshold"
+                    value="<?php echo esc_attr(intval(get_option('semonto_memory_usage_enable_warning_threshold', 90))); ?>"
+                    class="semonto_serverload"
+                />
+                <input
+                    type="number"
+                    min="0"
+                    max="100"
+                    name="semonto_memory_usage_enable_error_threshold"
+                    value="<?php echo esc_attr(intval(get_option('semonto_memory_usage_enable_error_threshold', 95))); ?>"
+                    class="semonto_serverload"
+                />
+            </div>
+        </div>
+    <?php else : ?>
+        <p class="semonto-health-monitor__not-available">
+            <span>ℹ️</span>
+            This test is not available due to your server setup.
+        </p>
+    <?php endif; ?>
+
 </div>
