182d2284f609b5fc005df9783e9e0900a6ea749d
J.T. Kalkman <j.t.kalkman@gmail.com>
2024-07-01 21:47:35 +0100

    Adds link to doc for the secret key.

--- semonto-health-monitor__secret-key.php
+++ semonto-health-monitor__secret-key.php
@@ -1,8 +1,14 @@
 <div class="semonto-health-monitor__test-container">
     <h3>Secret key</h3>
     <p>
-        If you want to add an extra layer of protection, copy the secret key from the
-        monitor settings in Semonto. This will limit exposure of your health endpoint.
+        If you want to add an extra layer of protection, copy the secret key from the 
+        monitor settings in Semonto. This will limit exposure of your health endpoint. 
+        Read more about in 
+        <a
+            target="_blank"
+            rel="noopener noreferrer" 
+            href="https://semonto.com/how-to/semonto-health-framework"
+        >our docs</a>. 
     </p>
     <div>
         <input 
