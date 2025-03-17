208bc2ff8cb31d6cdf67e89b7da85f3cd82de700
J.T. Kalkman <j.t.kalkman@gmail.com>
2025-03-17 08:05:49 +0000

    Updates version number.

--- semonto-health-monitor.php
+++ semonto-health-monitor.php
@@ -2,7 +2,7 @@
 
 /*
 Plugin Name: Semonto Health Monitor
-Version: 1.1.2
+Version: 1.1.3
 Description: Semonto health plugin for WordPress 
 Author: Coding Mammoth - Semonto
 License: GPLv2 or later
