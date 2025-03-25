e7940d78d5aaa548b88fcbc77c5b00a6c77f6b31
JTKalkman <JTKalkman@users.noreply.github.com>
2025-03-25 15:44:17 +0100

    Version 1.1.4 (#7)
    * Filters out loop and other devices in disk configuration. Adds check for malformed data.
    * Updates version number. Updates changelog.
    * Improves the filtering of disks.

--- functions.php
+++ functions.php
@@ -453,7 +453,9 @@ function semonto_get_available_disks ()
     for ($i = 1; $i < count($lines); $i++) {
         if (trim($lines[$i]) != '') {
             $cols = preg_split('/\s+/', $lines[$i]);
-            if (preg_match('/^\/dev\//', $cols[0])) {
+            if (!isset($cols[0])) { continue; }
+
+            if (preg_match('/^\/dev\/(?!loop|ram|vd|fd|sr|shm)\w+/', $cols[0])) {
                 $disks[$cols[0]] = [
                     "warning_percentage_threshold" => 75,
                     "error_percentage_threshold" => 90,
